3be03a4c09cd0e77b68f094f176e93ea35eb3674
f1zm0 <root@fzm.ooo>
2023-09-29 14:59:07 -0700

    chore: move lazynvm func to separate script

--- scripts/zsh_history_fix.sh
+++ scripts/zsh_history_fix.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 echo "[+] Recovering corrupted zsh_history file ..."
 mv ${HOME}/.zsh_history ${HOME}/.zsh_history_corrupted
 strings -eS ${HOME}/.zsh_history_corrupted > ${HOME}/.zsh_history
